3555a564028fe5467723c959887640276605bc2c
FredeAlexandre <alexandre.frede@epitech.eu>
2023-02-12 12:07:06 +0100

    finish the hud connection

--- Assets/Scripts/Player_Manager.cs
+++ Assets/Scripts/Player_Manager.cs
@@ -7,29 +7,70 @@ using TMPro;
 public class Player_Manager : MonoBehaviour
 {
     public int killCounter = 0;
-    public TMP_Text killCounterOutput;
 
     public bool showKillsInHud = true;
 
     public int hearts = 8;
 
     [Range(1f, 100f)]
-    public float mana = 100f;
+    public float maxMana = 100f;
+
+    public float fullManaDurationInSeconds = 30f;
+
+    private float manaConsomation;
+    
+    private float mana;
+
+    public TMP_Text killCounterOutput;
+    public Slider heartsOutput;
+    public Slider manaOutput;
+
+    private Player_Movement movements;
+
+    public bool canRun = true;
+    public bool isDead = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        movements = GetComponent<Player_Movement>();
+        manaConsomation = 100f / fullManaDurationInSeconds;
+        mana = maxMana;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (movements.isRunning) {
+            mana = mana - manaConsomation * Time.deltaTime;
+            if (mana <= 0) {
+                canRun = false;
+                mana = 0;
+            }
+            Debug.Log("mana = " + mana);
+            manaOutput.value = mana / 100f;
+        }
+    }
+
+    public void onDrug()
+    {
+        canRun = true;
+        mana = maxMana;
+        manaOutput.value = mana / 100f;
+    }
+
+    public void onDamage()
+    {
+        if (hearts == 0) return;
+        hearts--;
+        if (hearts == 0) isDead = true;
+        heartsOutput.value = hearts;
     }
 
     public void onKill()
     {
         killCounter++;
-        if (killCounterOutput != null) killCounterOutput.text = getKillString();
+        if (killCounterOutput != null && showKillsInHud) killCounterOutput.text = getKillString();
     }
 
     string getKillString()
