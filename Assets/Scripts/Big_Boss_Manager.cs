5ec4c03b6c867f663fd11a7a1d6ec266b2f80ed8
Mathematisse <lucien.pineau@epitech.eu>
2023-02-12 17:25:20 +0100

    Boss start

---
+++ Assets/Scripts/Big_Boss_Manager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Big_Boss_Manager : MonoBehaviour
+{
+    public float time_for_dog_spawn = 5f;
+    private float dog_timer;
+    public GameObject Dog;
+    Player_Movement player;
+    private float spawn_dist = 5f;
+    private Vector2 rdm_Dir;
+    private float rdm_Dist;
+    private Vector2 to_rdm_Dir;
+    private Rigidbody2D rb;
+    private float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player_Movement>();
+        dog_timer = time_for_dog_spawn;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move_Rdm_Values();
+        if (dog_timer > 0)
+        {
+            dog_timer -= Time.deltaTime;
+        }
+        else
+        {
+            dog_timer = time_for_dog_spawn;
+            Spawn_Dog_Randomly();
+        }
+    }
+    
+    void Spawn_Dog_Randomly()
+    {
+        var pos = new Vector3(Random.Range(-spawn_dist, spawn_dist), Random.Range(-spawn_dist, spawn_dist), 0);
+        var dog = Instantiate(Dog, pos + transform.position, Quaternion.identity, transform);
+        dog.GetComponent<Dog_Manager>().player = player.transform;
+    }
+
+    private void Move_Rdm_Values()
+    {
+        rdm_Dir += new Vector2(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f)) ;
+        rdm_Dir.Normalize();
+        rdm_Dist += Random.Range(-1, 1);
+        if (rdm_Dist > 15)
+        {
+            rdm_Dist = 15;
+        }
+        else if (rdm_Dist < 5)
+        {
+            rdm_Dist = 5;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        var pos = player.transform.position + (Vector3)rdm_Dir * rdm_Dist;
+        to_rdm_Dir = pos - transform.position;
+        to_rdm_Dir.Normalize();
+        rb.AddForce(to_rdm_Dir * speed);
+        //rb.velocity = ((to_rdm_Dir * speed) + rb.velocity) / 2f;
+        Debug.DrawLine(transform.position, pos, Color.red);
+    }
+}
